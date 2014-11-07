9a31bd2284cbbeaf601c438fdc99a5da3267f3c9
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-11-07 20:31:26 +0900

    [bugfix] eq(X,X) broken

--- Eval.hs
+++ Eval.hs
@@ -1,10 +1,14 @@
 module Eval where
 
 import Prelude hiding (head, tail)
+import Data.List hiding (head)
+import Data.Function
 import Kaguya
 
 compose :: Substitution -> Substitution -> Substitution
-compose lhs rhs = [ (var, subst lhs term) | (var, term) <- rhs ] ++ lhs
+compose lhs rhs = nubBy ((==) `on` fst) alist
+  where
+    alist = [ (var, subst lhs term) | (var, term) <- rhs ] ++ lhs
 
 subst :: Substitution -> Term -> Term
 subst theta (Compound op args) = Compound op (map (subst theta) args)
@@ -16,12 +20,14 @@ subst theta (Variable n) =
 unify :: Term -> Term -> Maybe Substitution
 unify (Variable var) term = return $ [(var, term)]
 unify term (Variable var) = return $ [(var, term)]
-unify (Compound op1 args1) (Compound op2 args2) | op1 == op2 && length args1 == length args2 = go (zip args1 args2) []
+unify (Compound op1 args1) (Compound op2 args2) | op1 == op2 = go args1 args2 []
   where
-    go [] theta = return $ theta
-    go ((t1,t2):cs) theta = do
+    go [] [] theta = return $ theta
+    go (t1:ts1) (t2:ts2) theta = do
       phi <- unify t1 t2
-      go cs (phi `compose` theta)
+      go (map (subst phi) ts1) (map (subst phi)ts2) (phi `compose` theta)
+    go [] _ _ = Nothing
+    go _ [] _ = Nothing
 unify _ _ = Nothing
 
 resolve :: [Clause] -> Term -> [Substitution]
@@ -34,4 +40,4 @@ resolve rules term = do
     go [] theta = return $ theta
     go (g:gs) theta = do
       phi <- resolve rules g
-      go gs (phi `compose` theta)
+      go (map (subst phi) gs) (phi `compose` theta)
