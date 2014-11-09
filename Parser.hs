82b94214f2c3175f795e48278db1a1868f385610
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-11-09 22:20:15 +0900

    lexeme parsers

--- Parser.hs
+++ Parser.hs
@@ -6,6 +6,22 @@ import Type
 
 type Parser t = Parsec String () t
 
+-- lex
+
+lexeme :: Parser t -> Parser t
+lexeme p = do
+  t <- p
+  whiteSpace
+  return t
+
+whiteSpace :: Parser ()
+whiteSpace = spaces
+
+op :: String -> Parser String
+op name = lexeme $ string name
+
+-- parse
+
 parse :: SourceName -> String -> Either ParseError [Clause]
 parse = runParser program ()
 
@@ -13,54 +29,52 @@ parseTerm :: String -> Either ParseError Term
 parseTerm = runParser term () ""
 
 program :: Parser [Clause]
-program = many (between spaces spaces clause)
+program = whiteSpace >> many clause
 
-atom :: Parser String
-atom = do
+functor :: Parser String
+functor = do
   head <- lower
   tail <- many (alphaNum <|> char '_')
   return (head : tail)
 
 arguments :: Parser [Term]
-arguments = option [] (do
-  char '('
-  args <- (between spaces spaces term) `sepBy` (char ',')
-  char ')'
-  return args)
+arguments = option [] $ do
+  op "("
+  args <- term `sepBy` (op ",")
+  op ")"
+  return args
 
 simpleCompound :: Parser Term
 simpleCompound = do
-  functor <- atom
-  args    <- option [] arguments
-  return $ Compound functor args
+  func <- functor
+  args <- option [] arguments
+  return $ Compound func args
 
 listCompound :: Parser Term
 listCompound = do
-  char '['
-  args <- (between spaces spaces term) `sepBy` (char ',')
-  rest <- option empty $ char '|' >> (between spaces spaces term)
-  char ']'
+  op "["
+  args <- term `sepBy` (op ",")
+  rest <- option empty $ op "|" >> term
+  op "]"
   return $ foldr (\x y -> Compound "." [x,y]) rest args
 
 compound :: Parser Term
 compound = try simpleCompound <|> listCompound
 
 var :: Parser Term
-var = do
+var = lexeme $ do
   head <- upper <|> char '_'
   tail <- many (alphaNum <|> char '_')
   return $ Variable (head : tail)
 
 term :: Parser Term
-term = var <|> compound
+term = try var <|> compound
 
 rule :: Parser Clause
 rule = do
   head <- term
-  spaces
-  string ":-"
-  spaces
-  body <- (between spaces spaces term) `sepBy` (char ',')
+  op ":-"
+  body <- term `sepBy` (op ",")
   return $ Rule head body
 
 fact :: Parser Clause
@@ -71,6 +85,5 @@ fact = do
 clause :: Parser Clause
 clause = do
   c <- try rule <|> fact
-  spaces
-  char '.'
+  op "."
   return $ c
